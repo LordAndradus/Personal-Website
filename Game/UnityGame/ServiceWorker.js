f97b38ca71580bf9b8221b24fe9d13c9dab1a2b2
LordAndradus <LordAndradus@gmail.com>
2024-10-31 15:02:14 -0500

    Trying to get Unity to work

---
+++ Game/UnityGame/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Game Jam-1.0";
+const contentToCache = [
+    "Build/WebsiteGame.loader.js",
+    "Build/WebsiteGame.framework.js.unityweb",
+    "Build/WebsiteGame.data.unityweb",
+    "Build/WebsiteGame.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
